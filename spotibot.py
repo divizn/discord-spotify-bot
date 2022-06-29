d8ab9b62ba38979e6580dde42712da488db6d518
Hassan <hassanjaved186@gmail.com>
2022-06-29 23:46:08 +0100

    redid song command, added embeds, added details

--- spotibot.py
+++ spotibot.py
@@ -9,6 +9,7 @@ from discord.ext.commands import Bot
 bot_token = secrets.bot_token
 client_id = secrets.client_id
 client_secret = secrets.client_secret #sets discord bot token, spotify app client id and client secrets 
+spotifyLogo = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/512px-Spotify_icon.svg.png"
 
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
@@ -21,19 +22,48 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="Use !cmds"))
     print(bot.user.name+" is active")
 
+
+
 @bot.command()
 #*args takes in the arguments passed to the bot (e.g. !search lil uzi will take lil and uzi) and then this method will return 5 songs from the arguments
 async def song(ctx, *args):
     SEARCH_TERM = " ".join(args) #turns the args into a search term to be used 
     results = sp.search(q=SEARCH_TERM, type='track', limit=5) #gets top 5 tracks from search query
-    urlsToSend = "First 5 results:"
-    for item in results['tracks']['items']:
-         urlsToSend += "\n"+item['external_urls']['spotify']
-    if urlsToSend == "First 5 results:":
-        await ctx.send(f"No results for {SEARCH_TERM}") #error handling for when there are no args
+    #get song name
+    songName = results['tracks']['items'][0]['name']
+    #gets song image
+    imageURL = results['tracks']['items'][0]['album']['images'][0]['url']
+    #get artist(s) names
+    artistNamesArr = []
+    #get song URL
+    songURL = results['tracks']['items'][0]['external_urls']['spotify']
+    #get popularity index 
+    popularityIndex = results['tracks']['items'][0]['popularity']
+    #check if song is explicit (true if yes)
+    isExplicit = results['tracks']['items'][0]['explicit'] 
+    #gets artist(s) names
+    for artists in results['tracks']['items'][0]['artists']:
+        artistNamesArr.append(artists['name'])
+    artistNames = ', '.join(map(str, artistNamesArr))
+    if results['tracks']['items'] == []:
+        embed=discord.Embed(color=0x00cc03)
+        embed.set_thumbnail(url=spotifyLogo)
+        embed.add_field(name="No results found", value=f"No results found for '{SEARCH_TERM}', please try again and make sure the song name is valid", inline=False)
+        await ctx.send(embed=embed)
     else:
-        await ctx.send(f"You searched for: {SEARCH_TERM}\n"+urlsToSend)
+        embed=discord.Embed(title=songName, url=songURL, color=0x00cc03)
+        embed.set_thumbnail(url=imageURL)
+        embed.add_field(name="Song name", value=songName, inline=True)
+        embed.add_field(name="Artist(s)", value=artistNames, inline=False)
+        if isExplicit:
+            embed.add_field(name="Explicit?", value="Explicit", inline=False)
+        else:
+            embed.add_field(name="Explicit?", value="Not Explicit", inline=False)
+        embed.add_field(name="Popularity index (0-100):", value=popularityIndex, inline=False)
+        await ctx.send(embed=embed)
     return
+
+
 
 @bot.command()
 #this command shows an artists profile
@@ -43,13 +73,18 @@ async def artist(ctx, *args):
     if results['artists']['items'] == []:
         await ctx.send(f"No results for {SEARCH_TERM}")  #check if no artists
     json = results['artists']['items']
+    #get artist details
     artistLink = json[0]['external_urls']['spotify']
     artistImage = json[0]['images'][0]['url']
     followers = json[0]['followers']['total']
     genresArr = json[0]['genres']
-    genres = ', '.join(map(str, genresArr))
+    if genresArr == []:
+        genres = "(No genres specified)"
+    else:
+        genres = ', '.join(map(str, genresArr))
     popularityIndex = str(json[0]['popularity'])
     artistName = json[0]['name']
+    #put details in discord embed
     embed=discord.Embed(title=artistName, url=artistLink, color=0x00cc03)
     embed.set_thumbnail(url=artistImage)
     # embed.add_field(name=artistName, value=artistLink, inline=False)
@@ -63,8 +98,11 @@ async def artist(ctx, *args):
 @bot.command()
 #this command shows the list of commands when !cmds is typed
 async def cmds(ctx):
-    await ctx.send("Use !song <search-tag> to get a song\nUse !artist <search-tag> to get an artist")
-    return
+    embed=discord.Embed(title="Commands", color=0x00cc03)
+    embed.set_thumbnail(url=spotifyLogo)
+    embed.add_field(name="Search for a song", value="!song [song name]", inline=False)
+    embed.add_field(name="Search for an artist", value="!artist [artist name]", inline=False)
+    await ctx.send(embed=embed)
 
 
 bot.run(bot_token)
