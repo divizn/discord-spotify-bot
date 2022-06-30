e2fe386bfe4190c4e2e02803f01b58807a199ca3
Hassan <hassanjaved186@gmail.com>
2022-06-30 04:27:05 +0100

    gitignore

--- secrets.py
+++
@@ -1,3 +0,0 @@
-client_id = 'df4c758cee1b4b7298edabf5793147e0'
-client_secret = '5d796082e7334e7791935d3a8aae35d9'
-bot_token = 'OTkwMjE3ODYwNjExNzExMDQ2.GvT2IR.TlyhHR62081cg1Pv5taOD0V3fxvku9-E5yP_VY'
