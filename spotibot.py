f8ec7c596b052fed6c002aec2cf1fc104b2eb925
Hassan <hassanjaved186@gmail.com>
2022-07-01 20:52:41 +0100

    added !clear and !spotify

--- spotibot.py
+++ spotibot.py
@@ -4,6 +4,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from secrets import client_id, client_secret, bot_token
 import discord
 from discord.ext.commands import Bot
+from discord import Spotify
 
 bot_token = secrets.bot_token
 client_id = secrets.client_id
@@ -12,13 +13,15 @@ spotifyLogo = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify
 
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
-bot = Bot(command_prefix = '!') #sets discord bot prefix to '!'
-                             
+intents = discord.Intents.all()
+bot = Bot(command_prefix = '!', intents=intents) #sets discord bot prefix to '!' and set intents
+
+
 
 @bot.event
 #when bot starts (on_ready), presence of the bot is changed to "Use !cmds" and it shows when the bot is active in the terminal
 async def on_ready():
-    await bot.change_presence(activity=discord.Game(name="Use !cmds"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Spotify"))
     print(bot.user.name+" is active")
 
 
@@ -89,7 +92,6 @@ async def artist(ctx, *args):
     #put details in discord embed
     embed=discord.Embed(title=artistName, url=artistLink, color=0x00cc03)
     embed.set_thumbnail(url=artistImage)
-    # embed.add_field(name=artistName, value=artistLink, inline=False)
     embed.add_field(name="Follower count:", value=followers, inline=False)
     embed.add_field(name="Genres:", value=genres, inline=True)
     embed.add_field(name="Popularity index (0-100):", value=popularityIndex, inline=False)
@@ -105,6 +107,38 @@ async def cmds(ctx):
     embed.add_field(name="Search for a song", value="!song [song name]", inline=False)
     embed.add_field(name="Search for an artist", value="!artist [artist name]", inline=False)
     await ctx.send(embed=embed)
+
+
+
+@bot.command()
+#this command will clear (amount) messages from a channel 
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+
+
+
+@bot.command()
+#shows information about the song the user is currently playing (requires discord account to be connected to a spotify account)
+async def spotify(ctx, user: discord.Member = None):
+    user = user or ctx.author
+    spotify_result = next((activity for activity in user.activities if isinstance(activity, discord.Spotify)), None)
+    if spotify_result is None:
+        embed=discord.Embed(title="Error has occured", color=0x00cc03)
+        embed.add_field(name=user, value=f"{user.display_name} is not listening to spotify")
+        embed.set_thumbnail(url=spotifyLogo)
+        await ctx.send(embed=embed)
+        return
+
+    embed = discord.Embed(
+        title = f"{user.name}'s Spotify",
+        description = "Listening to {}".format(spotify_result.title),
+        color = 0x00cc03)
+    embed.set_thumbnail(url=spotify_result.album_cover_url)
+    embed.add_field(name="Artist", value=spotify_result.artist)
+    embed.add_field(name="Album", value=spotify_result.album)
+    embed.set_footer(text="Song started at {}".format(spotify_result.created_at.strftime("%H:%M")))
+    await ctx.send(embed=embed)
+
 
 
 bot.run(bot_token)
